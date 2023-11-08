2754d11649bb1983b0f5a508ecd479f0def001f6
Nulljayden <99763670+Nulljayden@users.noreply.github.com>
2023-11-08 08:33:52 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Google Drive</title>
+        <link rel="icon" href="https://logodownload.org/wp-content/uploads/2020/04/google-drive-logo-0-1.png" />
       </Head>
 
       <main>
