f4d15571db40655cc5a79bdf50caab4a68d75289
Nulljayden <99763670+Nulljayden@users.noreply.github.com>
2023-11-08 08:48:00 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Google Drive</title>
-        <link rel="icon" href="/google-drive-logo-0-1.png" />
+        <link rel="icon" href="/google-drive-logo-0-1.png" width=800 height=600/>
       </Head>
 
       <main>
